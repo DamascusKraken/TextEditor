91c75f47f0c70744159e99cbf95b428dbc965cf0
DamascusKraken <nick.canchola.7.27@gmail.com>
2024-01-14 19:48:05 -0600

    Added catch to eventListenter. Completed eventlistenter for appinstalled.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,33 +6,39 @@ let deferredPrompt;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-    //Prevent the default prompt
-    event.preventDefault();
+  //Prevent the default prompt
+  event.preventDefault();
 
-    //Store the event for later use
-    deferredPrompt = event;
+  //Store the event for later use
+  deferredPrompt = event;
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
-    //Validate if deferredPrompt is available
-    if(deferredPrompt) {
-        try {
-            //Manual trigger of prompt
-            await deferredPrompt.prompt();
+  //Validate if deferredPrompt is available
+  if (deferredPrompt) {
+    try {
+      //Manual trigger of prompt
+      await deferredPrompt.prompt();
 
-            //Dont know how long it will take for a response. Wait for user to respond
-            const choiceResult = await deferredPrompt.userChoice;
+      //Dont know how long it will take for a response. Wait for user to respond
+      const choiceResult = await deferredPrompt.userChoice;
 
-            if(choiceResult.outcome === "accepted") {
-                console.log("User accepted the installation");
-            }else {
-                console.log("User declined the installation");
-            }
-        }
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the installation");
+      } else {
+        console.log("User declined the installation");
+      }
+
+      //Reset deferredPrompt after handling
+      deferredPrompt = null;
+    } catch (error) {
+      console.error("Error during Installation process: ", error);
     }
-
+  }
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log(" App installed Successfully!");
+});
