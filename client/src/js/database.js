122126453a312bd32266094d1a018545f0accdeb
DamascusKraken <nick.canchola.7.27@gmail.com>
2024-01-14 22:16:28 -0600

    getDb logic done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,20 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
-const text = db.transaction("jate", "readwrite");
-const saveContent = text.objectStore("jate");
+export const putDb = async (content) => {
+  const db = await initdb();
+  const text = db.transaction("jate", "readwrite");
+  const saveContent = text.objectStore("jate");
 
-// Add userContent to the database
-const userInput = await saveContent.add({ content });
+  // Add userContent to the database
+  const userInput = await saveContent.add({ content });
 
-//Display userInput
-console.log("Added to DB: ", userInput);
+  //Display userInput
+  console.log("Added to DB: ", userInput);
 
-//Complete Save
-await text.done;
+  //Complete Save
+  await text.done;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  const db = await initdb();
+  const text = db.transaction("jate", "readonly");
+  const saveContent = text.objectStore("jate");
+
+  //Retrieve all content from database
+  const savedContent = await StorageEvent.getAll();
+
+  //Log past content
+  console.log("All content from DB: ", savedContent);
+
+  //complete transaction
+  await text.done;
+
+  return savedContent;
+};
 
 initdb();
